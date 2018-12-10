0e42f6742305649dee45bc39d461c71ae1c6febc
fillipvt <afvasquezt@gmail.com>
2018-12-10 11:57:20 -0500

    new README

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
+1. Clone the repo
+2. `cd` into the repo folder
+3. run `now`
+
 WIP
--- www/blog/src/pages/index.js
+++ www/blog/src/pages/index.js
@@ -9,6 +9,7 @@ const IndexPage = () => (
     <h1>Hell to the yeah people!!!!!</h1>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
+    <p>It works!</p>
     <div style={{ maxWidth: '300px', marginBottom: '1.45rem' }}>
       <Image />
     </div>
