e5d902388c1783928dc54c73c76505cdbb1dd88e
fillipvt <afvasquezt@gmail.com>
2019-02-16 22:54:44 -0500

    implement transpilation of external packages

---
+++ packages/next-blog/next.config.js
@@ -0,0 +1,8 @@
+const withTM = require('next-transpile-modules')
+const isProd = process.env.NODE_ENV === 'production'
+
+module.exports = withTM({
+  // You may only need to add assetPrefix in the production.
+  transpileModules: ['@fillipvt/components'],
+  target: 'serverless',
+})
