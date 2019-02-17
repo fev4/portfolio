f8865ab746ee62148c2355bf58de323792fa9a96
fillipvt <afvasquezt@gmail.com>
2019-02-16 22:53:16 -0500

    New location for files

---
+++ packages/next-main/lib/fontawesome.js
@@ -0,0 +1,23 @@
+/** Importar en este archivo todos los íconos que se vayan a utilizar */
+import { library } from '@fortawesome/fontawesome-svg-core'
+import {
+  faBars,
+  faCopyright,
+  faBalanceScale,
+  faPiggyBank,
+  faRoute,
+  faBinoculars,
+} from '@fortawesome/free-solid-svg-icons'
+import { } from '@fortawesome/free-regular-svg-icons'
+import { faGoogle } from '@fortawesome/free-brands-svg-icons'
+
+/* Font Awesome Icons */
+library.add(
+  faBars,
+  faCopyright,
+  faBalanceScale,
+  faPiggyBank,
+  faRoute,
+  faBinoculars,
+  faGoogle
+)
