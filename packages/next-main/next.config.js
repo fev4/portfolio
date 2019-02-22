2e6a591dab3c715e92d3add68321d1b4fbcd9627
fillipvt <afvasquezt@gmail.com>
2019-02-21 23:26:28 -0500

    remove polyfill configuration of webpack

--- packages/next-main/next.config.js
+++ packages/next-main/next.config.js
@@ -2,22 +2,5 @@ const withTM = require('next-transpile-modules')
 
 module.exports = withTM({
   transpileModules: ['@fillipvt/components'],
-  target: 'serverless',
-  webpack: function (cfg) {
-    const originalEntry = cfg.entry
-    cfg.entry = async () => {
-      const entries = await originalEntry()
-
-      if (
-        entries['main.js'] &&
-        !entries['main.js'].includes('./client/polyfills.js')
-      ) {
-        entries['main.js'].unshift('./client/polyfills.js')
-      }
-
-      return entries
-    }
-
-    return cfg
-  }
+  target: 'serverless'
 })
